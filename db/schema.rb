4ec56654b4a0ba5d30a9e4f586b0b0b786ba2d4c
Man Leung <lepusleung@gmail.com>
2018-06-07 13:46:22 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 20180607133923) do
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
